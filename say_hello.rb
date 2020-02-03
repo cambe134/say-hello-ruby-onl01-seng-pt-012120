dac7316002c6c42ae6c5f0443ed58b6b5514627e
Spencer Cambern <cambern134@gmail.com>
2020-02-03 08:25:26 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,4 @@
-# I call on the method, say_hello, and give it the string "Gabriela"
-say_hello("Kent Beck")
 
-# The method prints this text to the screen:
-Hello Kent Beck!
+def say_hello("Kent Beck")
+ phrase = "Hello Kent Beck!"
+end
