5c346c636286f0e5c50489f955b7701c59c19c5f
Spencer Cambern <cambern134@gmail.com>
2020-02-03 08:24:44 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
-# I call on the method, say_hello, and give it the string "Gabriela" 
+# I call on the method, say_hello, and give it the string "Gabriela"
 say_hello("Kent Beck")
 
 # The method prints this text to the screen:
